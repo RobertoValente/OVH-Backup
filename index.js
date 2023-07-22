889fed68cbea93a1bf8f8cfa4787894c03c5194c
Roberto Valente <73560955+RobertoValente@users.noreply.github.com>
2023-07-22 21:08:22 +0100

    Changing the messages to English

--- index.js
+++ index.js
@@ -12,15 +12,15 @@ const ovh = require('ovh')({
 });
 
 cron.schedule('0 15 5 * * *', () => {
-    let msg = `> CronJob de Hora do Backup!`;
-    let cronType = 'SpecificHour';
+    let msg = `> Creating Backup!`;
+    let cronType = 'BackupHour';
 
     sendWhatsappMessage(msg, cronType);
 }, {scheduled: true, timezone: 'Europe/Lisbon'});
 
 cron.schedule('0 0 * * * *', () => {
-    let msg = `> CronJob de Hora em Hora!`;
-    let cronType = 'EveryHour';
+    let msg = `> Status: X`;
+    let cronType = 'StatusVPS';
 
     sendWhatsappMessage(msg, cronType);
 }, {scheduled: true, timezone: 'Europe/Lisbon'});
@@ -28,9 +28,9 @@ cron.schedule('0 0 * * * *', () => {
 function sendWhatsappMessage(msg, cronType) {
     axios.post(`http://api.callmebot.com/whatsapp.php?source=web&phone=${process.env.PHONE}&apikey=${process.env.KEY}&text=${encodeURI(msg)}`)
     .then(res => {
-        console.log(`[${new Date().toLocaleString('pt', {timeZone: 'Europe/Lisbon'})}] CronJob ${cronType} > Mensagem Enviada!`);
+        console.log(`[${new Date().toLocaleString('pt', {timeZone: 'Europe/Lisbon'})}] CronJob ${cronType} > Message Sent!`);
     })
     .catch(err => {
-        console.log(`[${new Date().toLocaleString('pt', {timeZone: 'Europe/Lisbon'})}] CronJob ${cronType} > Erro: ${err.message}`);
+        console.log(`[${new Date().toLocaleString('pt', {timeZone: 'Europe/Lisbon'})}] CronJob ${cronType} > Error: ${err.message}`);
     });
 }
