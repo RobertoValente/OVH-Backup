a2361bc932a338c4140ef1bf82aa9f7758553ad2
Roberto Valente <73560955+RobertoValente@users.noreply.github.com>
2023-07-24 13:06:08 +0100

    Added the commands to install ftp

--- setupRaspberry.sh
+++ setupRaspberry.sh
@@ -6,11 +6,9 @@ sudo apt update -y && sudo apt upgrade -y
 sudo apt install nodejs -y
 sudo apt install npm -y
 sudo apt install git -y
+sudo apt install vsftpd -y
 echo '/* =============================================== */'
 echo '/* =============================================== */'
-
-echo ' '
-echo ' '
 
 echo '/* =============================================== */'
 echo '/* ================== HD Setup =================== */'
@@ -26,15 +24,39 @@ echo '> n + Press <Enter> + Press <Enter> + Press <Enter>'
 echo '> w'
 echo '/* =============================================== */'
 
+read -p 'Insert your Username: ' username
 sudo fdisk /dev/$disk
 read -p 'Folder Name of the Disk: ' folderdisk
 sudo mkfs.exfat -n $folderdisk /dev/$disk
 df -h
 sudo mkdir /media/$folderdisk
-read -p 'Username: ' username
 sudo chown -R $username:$username /media/$folderdisk
 sudo mount /dev/$disk /media/$folderdisk/ -o uid=$username,gid=$username
 lsblk
+echo ' '
+echo ' '
+
+echo '/* =============================================== */'
+echo '/* ================== FTP Setup ================== */'
+echo ' '
+echo '> To continue the script, create another session and follow the instructions:'
+echo '> Run the command: mkdir -p /media/'$folderdisk'/FTP'
+echo '> Run: sudo nano /etc/vsftpd.conf'
+echo '> Uncomment the following lines: '
+echo '   > write_enable=YES'
+echo '   > local_umask=022'
+echo '   > chroot_local_user=YES'
+echo '> Change the line: anonymous_enable=YES to anonymous_enable=NO'
+echo '> Add the following lines: '
+echo '   > user_sub_token=$USER'
+echo '   > local_root=/media/'$folderdisk'/FTP'
+echo ' '
+echo '/* =============================================== */'
+echo '/* =============================================== */'
+
+echo ' '
+echo ' '
+sudo service vsftpd restart
 df -h
 sudo umount /media/$folderdisk
 ls -l /dev/disk/by-uuid/
