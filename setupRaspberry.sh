3eccc566900c6e0e9f43f1aeac56587ca526090d
Roberto Valente <73560955+RobertoValente@users.noreply.github.com>
2023-07-25 21:27:32 +0100

    Script renovated (without hd formatation code)

--- setupRaspberry.sh
+++ setupRaspberry.sh
@@ -1,5 +1,69 @@
 #!/bin/bash
 
+echo "Installing the necessary packages..."
+
+sudo apt update -y && sudo apt upgrade -y > /dev/null
+echo "Packages has been updated and upgraded"
+
+sudo apt install nodejs -y > /dev/null
+echo "Package NodeJS Installed"
+
+sudo apt install npm -y > /dev/null
+echo "Package NPM Installed"
+
+sudo apt install git -y > /dev/null
+echo "Package GIT Installed"
+
+sudo apt install vsftpd -y > /dev/null
+echo "Package VSFTPD Installed"
+
+sudo apt install ntfs-3g -y > /dev/null
+echo "Package NTFTS-3G Installed"
+
+echo " "
+echo " "
+
+read -p "Insert your username: " username
+read -p "Insert the name of HD's Folder: " folderdisk
+
+sudo mkdir /media/$folderdisk
+echo "Folder Created at: /media/" $folderdisk "/"
+
+lsblk
+read -p "Insert the name of HD Partition to be mounted: " partition
+sudo mount -t ntfs-3g /dev/$partition /media/$folderdisk
+
+echo " "
+echo " "
+
+echo "If you want to mount this device automatically in Operative System Startup:"
+echo "> Run the command: ls -l /dev/disk/by-uuid/"
+echo "> Get the UUID of the device you want for you substitute the 'DEVICE_UUID_HERE' with it"
+echo "> Run the command: sudo nano /etc/fstab"
+echo "> Add to the end of the file: UUID=DEVICE_UUID_HERE /media/"$folderdisk"/ ntfs-3g auto,nofail,noatime,users,rw 0 0"
+
+echo " "
+echo " "
+
+echo "To Turn On FTP:"
+echo "> Run the command: sudo mkdir -p /media/"$folderdisk"/FTP"
+echo "> Run the command: sudo nano /etc/vsftpd.conf"
+echo "> Uncomment the following lines: "
+echo "   > write_enable=YES"
+echo "   > local_umask=022"
+echo "   > chroot_local_user=YES"
+echo "> Change the line: anonymous_enable=YES to anonymous_enable=NO"
+echo "> Add the following lines: "
+echo "   > user_sub_token="$USER""
+echo "   > local_root=/media/"$folderdisk"/FTP"
+echo "> Run the command: sudo service vsftpd restart"
+
+echo " "
+echo " "
+
+echo "Setup Finished!"
+
+<<com
 echo '/* =============================================== */'
 echo '/* == Updating and Installing What Is Necessary == */'
 sudo apt update -y && sudo apt upgrade -y
@@ -7,6 +71,7 @@ sudo apt install nodejs -y
 sudo apt install npm -y
 sudo apt install git -y
 sudo apt install vsftpd -y
+sudo apt install ntfs-3g -y
 echo '/* =============================================== */'
 echo '/* =============================================== */'
 
@@ -17,19 +82,21 @@ lsblk
 read -p 'Disk: ' disk
 mount | grep $disk
 
+read -p 'Insert your Username: ' username
+read -p 'Folder Name of the Disk: ' folderdisk
+
+sudo fdisk /dev/$disk
 echo '/* === Type The Letters In The Following Order === */'
 echo '> g'
 echo '> p'
 echo '> n + Press <Enter> + Press <Enter> + Press <Enter>'
 echo '> w'
 echo '/* =============================================== */'
-
-read -p 'Insert your Username: ' username
-sudo fdisk /dev/$disk
-read -p 'Folder Name of the Disk: ' folderdisk
-sudo mkfs.exfat -n $folderdisk /dev/$disk
 df -h
-sudo mkdir /media/$folderdisk
+
+sudo mkdir /media/$folderdisk3g
+sudo mkfs.exfat -n $folderdisk /dev/$disk
+
 sudo chown -R $username:$username /media/$folderdisk
 sudo mount /dev/$disk /media/$folderdisk/ -o uid=$username,gid=$username
 lsblk
@@ -65,9 +132,10 @@ echo ' '
 echo ' '
 echo '/* ================== To Finish ================== */'
 echo '> sudo nano /etc/fstab'
-echo 'Paste this: UUID='$uuid' /media/'$folderdisk'/ exfat auto,nofail,noatime,users,rw,uid='$username',gid='$username' 0 0'
+echo 'Paste this: UUID='$uuid' /media/'$folderdisk'/ ntfs-3g auto,nofail,noatime,users,rw,uid='$username',gid='$username' 0 0'
 echo 'Press: Ctrl + X, Ctrl + Y, Enter'
 echo '> sudo reboot'
 echo 'To check everything, execute the following command to see if the system recognize the hard drive after it turns on:'
 echo 'cd /media/'$folderdisk
 echo '/* =============================================== */'
+com
